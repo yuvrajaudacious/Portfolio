82d5dc7ab14f6d47d5deb264427f59b553f8832b
yuvrajaudacious <audaciousyuvraj227@gmail.com>
2024-06-26 11:15:29 +0530

    resolve error

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,4 +1,3 @@
-import Particles from "react-tsparticles"
 import { Container } from "./styles";
 import { Hero } from "../Hero/Hero";
 import { About } from "../About/About";
@@ -15,6 +14,7 @@ import reactIcon from "../../assets/react-icon.svg";
 import typescriptIcon from "../../assets/typescript-icon.svg";
 import vueIcon from "../../assets/vue-icon.svg";
 import boostrapIcon from "../../assets/bootstrap-icon.svg";
+import Particles from "react-tsparticles";
 
 export function Main() {
   return (
@@ -22,161 +22,161 @@ export function Main() {
       <Particles
         id="tsparticles"
         options={{
-          "fullScreen": {
-            "enable": true,
-            "zIndex": 1
+          fullScreen: {
+            enable: true,
+            zIndex: 1,
           },
-          "detectRetina": true,
-          "fpsLimit": 60,
-          "interactivity": {
-            "events": {
-              "onClick": {
-                "enable": true,
-                "mode": "push"
+          detectRetina: true,
+          fpsLimit: 60,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: true,
+                mode: "push",
               },
-              "onDiv": {
-                "elementId": "repulse-div",
-                "enable": false,
-                "mode": "repulse"
+              onDiv: {
+                elementId: "repulse-div",
+                enable: false,
+                mode: "repulse",
               },
-              "onHover": {
-                "enable": true,
-                "mode": "bubble",
-                "parallax": {
-                  "enable": false,
-                  "force": 60,
-                  "smooth": 10
-                }
-              },
-              "resize": true
-            },
-            "modes": {
-              "bubble": {
-                "distance": 400,
-                "duration": 2,
-                "opacity": 0.8,
-                "size": 2,
-              },
-              "connect": {
-                "distance": 80,
-                "lineLinked": {
-                  "opacity": 0.5
+              onHover: {
+                enable: true,
+                mode: "bubble",
+                parallax: {
+                  enable: false,
+                  force: 60,
+                  smooth: 10,
                 },
-                "radius": 60
               },
-              "grab": {
-                "distance": 400,
-                "lineLinked": {
-                  "opacity": 1
-                }
+              resize: true,
+            },
+            modes: {
+              bubble: {
+                distance: 400,
+                duration: 2,
+                opacity: 0.8,
+                size: 2,
               },
-              "push": {
-                "quantity": 2
+              connect: {
+                distance: 80,
+                lineLinked: {
+                  opacity: 0.5,
+                },
+                radius: 60,
               },
-              "remove": {
-                "quantity": 2
+              grab: {
+                distance: 400,
+                lineLinked: {
+                  opacity: 1,
+                },
               },
-              "repulse": {
-                "distance": 200,
-                "duration": 0.4
-              }
-            }
+              push: {
+                quantity: 2,
+              },
+              remove: {
+                quantity: 2,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              },
+            },
           },
-          "particles": {
-            "color": {
-              "value": "#ffffff"
+          particles: {
+            color: {
+              value: "#ffffff",
             },
-            "lineLinked": {
-              "blink": false,
-              "color": "#000",
-              "consent": false,
-              "distance": 150,
-              "enable": false,
-              "opacity": 0.4,
-              "width": 1
+            lineLinked: {
+              blink: false,
+              color: "#000",
+              consent: false,
+              distance: 150,
+              enable: false,
+              opacity: 0.4,
+              width: 1,
             },
-            "move": {
-              "attract": {
-                "enable": false,
-                "rotate": {
-                  "x": 600,
-                  "y": 1200
-                }
+            move: {
+              attract: {
+                enable: false,
+                rotate: {
+                  x: 600,
+                  y: 1200,
+                },
               },
-              "bounce": false,
-              "direction": "none",
-              "enable": true,
-              "outMode": "out",
-              "random": false,
-              "speed": 2,
-              "straight": false
+              bounce: false,
+              direction: "none",
+              enable: true,
+              outMode: "out",
+              random: false,
+              speed: 2,
+              straight: false,
             },
-            "number": {
-              "density": {
-                "enable": true,
-                "area": 800
+            number: {
+              density: {
+                enable: true,
+                area: 800,
               },
-              "limit": 20,
-              "value": 15,
+              limit: 20,
+              value: 15,
             },
-            "opacity": {
-              "animation": {
-                "enable": true,
-                "minimumValue": 0.2,
-                "speed": 1,
-                "sync": false
+            opacity: {
+              animation: {
+                enable: true,
+                minimumValue: 0.2,
+                speed: 1,
+                sync: false,
               },
-              "random": true,
-              "value": 1
+              random: true,
+              value: 1,
             },
-            "rotate": {
-              "animation": {
-                "enable": true,
-                "speed": 5,
-                "sync": false
+            rotate: {
+              animation: {
+                enable: true,
+                speed: 5,
+                sync: false,
               },
-              "direction": "random",
-              "random": true,
-              "value": 0
+              direction: "random",
+              random: true,
+              value: 0,
             },
-            "shape": {
-              "character": {
-                "fill": false,
-                "font": "Verdana",
-                "style": "",
-                "value": "*",
-                "weight": "400"
+            shape: {
+              character: {
+                fill: false,
+                font: "Verdana",
+                style: "",
+                value: "*",
+                weight: "400",
               },
-              "image": [
+              image: [
                 {
-                  "src": boostrapIcon,
-                  "width": 20,
-                  "height": 20
+                  src: boostrapIcon,
+                  width: 20,
+                  height: 20,
                 },
                 {
-                  "src": cssIcon,
-                  "width": 20,
-                  "height": 20
+                  src: cssIcon,
+                  width: 20,
+                  height: 20,
                 },
                 {
-                  "src": wordpress,
-                  "width": 20,
-                  "height": 20
+                  src: wordpress,
+                  width: 20,
+                  height: 20,
                 },
                 {
-                  "src": shopify,
-                  "width": 20,
-                  "height": 20
+                  src: shopify,
+                  width: 20,
+                  height: 20,
                 },
                 {
-                  "src": htmlIcon,
-                  "width": 20,
-                  "height": 20
+                  src: htmlIcon,
+                  width: 20,
+                  height: 20,
                 },
                 {
-                  "src": jsIcon,
-                  "width": 20,
-                  "height": 20
+                  src: jsIcon,
+                  width: 20,
+                  height: 20,
                 },
                 // {
                 //   "src": mysqlIcon,
@@ -184,24 +184,24 @@ export function Main() {
                 //   "height": 20
                 // },
                 {
-                  "src": nodeIcon,
-                  "width": 20,
-                  "height": 20
+                  src: nodeIcon,
+                  width: 20,
+                  height: 20,
                 },
                 {
-                  "src": reactIcon,
-                  "width": 20,
-                  "height": 20
+                  src: reactIcon,
+                  width: 20,
+                  height: 20,
                 },
                 {
-                  "src": sassIcon,
-                  "width": 20,
-                  "height": 20
+                  src: sassIcon,
+                  width: 20,
+                  height: 20,
                 },
                 {
-                  "src": typescriptIcon,
-                  "width": 20,
-                  "height": 20
+                  src: typescriptIcon,
+                  width: 20,
+                  height: 20,
                 },
                 // {
                 //   "src": vscodeIcon,
@@ -209,50 +209,49 @@ export function Main() {
                 //   "height": 20
                 // },
                 {
-                  "src": vueIcon,
-                  "width": 20,
-                  "height": 20
+                  src: vueIcon,
+                  width: 20,
+                  height: 20,
                 },
-
               ],
-              "polygon": {
-                "sides": 5
+              polygon: {
+                sides: 5,
               },
-              "stroke": {
-                "color": "#000000",
-                "width": 0
+              stroke: {
+                color: "#000000",
+                width: 0,
               },
-              "type": "image"
+              type: "image",
             },
-            "size": {
-              "animation": {
-                "enable": false,
-                "minimumValue": 0.1,
-                "speed": 40,
-                "sync": false
+            size: {
+              animation: {
+                enable: false,
+                minimumValue: 0.1,
+                speed: 40,
+                sync: false,
               },
-              "random": false,
-              "value": 16
-            }
+              random: false,
+              value: 16,
+            },
           },
-          "polygon": {
-            "draw": {
-              "enable": false,
-              "lineColor": "#ffffff",
-              "lineWidth": 0.5
+          polygon: {
+            draw: {
+              enable: false,
+              lineColor: "#ffffff",
+              lineWidth: 0.5,
             },
-            "move": {
-              "radius": 10
+            move: {
+              radius: 10,
             },
-            "scale": 1,
-            "url": ""
+            scale: 1,
+            url: "",
           },
-          "background": {
-            "image": "",
-            "position": "50% 50%",
-            "repeat": "no-repeat",
-            "size": "cover"
-          }
+          background: {
+            image: "",
+            position: "50% 50%",
+            repeat: "no-repeat",
+            size: "cover",
+          },
         }}
       />
       <Hero></Hero>
